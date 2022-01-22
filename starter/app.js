b5e19c32357576998c8c00d364b1491fd4acc9ef
jeetpatel24 <friend.jeet24@gmail.com>
2022-01-22 09:31:35 +0530

    basic project setup

---
+++ starter/app.js
@@ -0,0 +1,29 @@
+require('dotenv').config();
+require('express-async-errors');
+
+const express = require('express');
+const app = express();
+
+const notFoundMiddleware = require('./middleware/not-found');
+const errorHandlerMiddleware = require('./middleware/error-handler');
+
+// middleware
+app.use(express.static('./public'));  //to access front end files in 'public' folder
+app.use(express.json());              //to access request body sent from front end
+
+app.use(notFoundMiddleware);
+app.use(errorHandlerMiddleware);
+
+const port = process.env.PORT || 3000;
+
+const start = async () => {
+  try {
+    app.listen(port, () =>
+      console.log(`Server is listening on port ${port}...`)
+    );
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+start();
